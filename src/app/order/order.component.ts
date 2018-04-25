e429966f4f385424b34bbb82735145792c626170
lffelizardo <lfernandes.felizardo@gmail.com>
2018-04-25 19:08:13 -0300

    building total buy component; ending the buy (Http Post)

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -3,12 +3,16 @@ import { Component, OnInit } from '@angular/core';
 import { RadioOption } from '../shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
+import { Order, OrderItem } from './order.model';
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  //MOCK
+  delivery: number = 8;
 
   paymentOptions: RadioOption [] = [
     {label: 'Dinheiro', value: 'MON'},
@@ -19,6 +23,10 @@ export class OrderComponent implements OnInit {
   constructor(private orderService:OrderService) { }
 
   ngOnInit() {
+  }
+
+  itemsValue(): number{
+    return this.orderService.itemsValue();
   }
 
   cartItems(): CartItem[]{
@@ -35,6 +43,16 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem){
     this.orderService.remove(item);
+  }
+
+  checkOrder(order: Order){
+    order.orderItems = this.cartItems()
+    .map((item: CartItem) => new OrderItem(item.quantity,item.menuItem.id))
+    this.orderService.checkOrder(order).subscribe((orderId: string) => {
+      console.log(`Compra concluída: ${orderId}`);
+      this.orderService.clear();
+    });
+    console.log(order);
   }
 
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core'
+import { Http, Headers, RequestOptions } from '@angular/http'
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/operator/map'
 
 import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service'
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model'
+import { Order } from './order.model'
+
+import { MEAT_API } from '../app.api'
 
 
 @Injectable()
 export class OrderService{
 
-    constructor(private cartService: ShoppingCartService){}
+    constructor(private cartService: ShoppingCartService, private http: Http){}
 
     cartItems(): CartItem[]{
         return this.cartService.items
@@ -22,6 +28,24 @@ export class OrderService{
     }
 
     remove(item: CartItem){
-    this.cartService.removeItem(item);
-  }
+        this.cartService.removeItem(item);
+    }
+
+    itemsValue(): number{
+        return this.cartService.total();
+    }
+
+    checkOrder(order: Order): Observable<String>{
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+        return this.http.post(`${MEAT_API}/orders`,
+                                JSON.stringify(order),
+                                new RequestOptions({headers: headers}))
+                        .map(response => response.json());        
+    }
+
+    clear(){
+        this.cartService.clear();
+    }
+
 }
