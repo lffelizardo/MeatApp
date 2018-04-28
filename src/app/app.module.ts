1df739b81d53650b5963cefae592673003f2db35
lffelizardo <lfernandes.felizardo@gmail.com>
2018-04-28 12:32:14 -0300

    refactoring to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { SharedModule } from './shared/shared.module';
+
 import {ROUTES} from './app.routes';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { RestaurantComponent } from './restaurantes/restaurant/restaurant.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
@@ -18,24 +19,17 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewComponent } from './restaurant-detail/review/review.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 import { RestaurantsService } from './restaurantes/restaurants.service';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderService } from './order/order.service';
-import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
-import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantesComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
@@ -43,19 +37,12 @@ import { RatingComponent } from './shared/rating/rating.component';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewComponent,
-    OrderComponent,
-    InputComponent,
-    RadioComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent,
-    OrderSummaryComponent,
-    RatingComponent
+    OrderSummaryComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule,
-    ReactiveFormsModule,
+    SharedModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [RestaurantsService, OrderService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,16 @@
 import {Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
 import { MenuComponent } from './restaurant-detail/menu/menu.component'
 import { ReviewComponent } from './restaurant-detail/review/review.component'
-import { OrderComponent } from './order/order.component'
 import { OrderSummaryComponent } from './order-summary/order-summary.component'
 
 export const ROUTES : Routes = [
     {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
+    {path: 'about', loadChildren: './about/about.module#AboutModule'},
     {path: 'restaurants', component: RestaurantesComponent },
-    {path: 'order', component: OrderComponent},
+    {path: 'order', loadChildren: './order/order.module#OrderModule'},
     {path: 'order-summary', component: OrderSummaryComponent},
     {path: 'restaurants/:id', component: RestaurantDetailComponent,
         children: [
